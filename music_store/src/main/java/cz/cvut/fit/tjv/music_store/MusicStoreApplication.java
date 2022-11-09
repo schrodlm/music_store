9622feafc1f597835bcb57c53da0e833c78b65de
Matěj Schrödl <schromat@fit.cvut.cz>
2022-11-09 10:53:06 +0100

    Starting springboot project

---
+++ music_store/src/main/java/cz/cvut/fit/tjv/music_store/MusicStoreApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.music_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApplication.class, args);
+	}
+
+}
---
+++ music_store/src/test/java/cz/cvut/fit/tjv/music_store/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.music_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
