b9089076eb61088297ce5a1d99d7b4a086b99857
Matěj Schrödl <msamot@seznam.cz>
2023-01-11 19:28:10 +0100

    Added webclient

---
+++ music_store_client/src/main/java/cz/cvut/tjv/music_store_client/MusicStoreClientApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.tjv.music_store_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreClientApplication.class, args);
+	}
+
+}
---
+++ music_store_client/src/test/java/cz/cvut/tjv/music_store_client/MusicStoreClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.tjv.music_store_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
